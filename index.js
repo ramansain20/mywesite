aa3573f4e431be136dd9ef048356ba7d05fbde31
ramansain20 <20je0766@ece.iitism.ac.in>
2021-06-15 23:36:42 +0530

    third commit

--- index.js
+++ index.js
@@ -8,20 +8,39 @@ upArrow.addEventListener("click", () => {
     x[0].style.display = "none";
   }
 });
+// const frontEnd = document.getElementsByClassName("frontend")[0];
+// const backEnd = document.getElementsByClassName("backend")[0];
+// const frontArrow = document.getElementsByClassName("front_arrow")[0];
+// const backArrow = document.getElementsByClassName("back_arrow")[0];
+// frontArrow.addEventListener("click", () => {
+//   if (frontEnd.style.display === "none") {
+//     frontEnd.style.display = "block";
+//   } else {
+//     frontEnd.style.display == "none";
+//   }
+// });
+// backArrow.addEventListener("click", () => {
+//   if (backEnd.style.display === "none") {
+//     backEnd.style.display = "block";
+//   } else {
+//     backEnd.style.display == "none";
+//   }
+// });
 const frontEnd = document.getElementsByClassName("frontend")[0];
 const backEnd = document.getElementsByClassName("backend")[0];
-const frontArrow = document.getElementsByClassName("front_arrow")[0];
-const backArrow = document.getElementsByClassName("back_arrow")[0];
-frontArrow.addEventListener("click", () => {
+const div = document.getElementsByClassName("skills_set");
+const front = div[0];
+const back = div[1];
+front.addEventListener("click", () => {
   if (frontEnd.style.display === "none") {
-    frontEnd.style.display = "block";
+    frontEnd.style.display = "inline";
   } else {
     frontEnd.style.display == "none";
   }
 });
-backArrow.addEventListener("click", () => {
+back.addEventListener("click", () => {
   if (backEnd.style.display === "none") {
-    backEnd.style.display = "block";
+    backEnd.style.display = "inline";
   } else {
     backEnd.style.display == "none";
   }
